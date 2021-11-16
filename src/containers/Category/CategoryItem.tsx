a0df5a5b61361df2bdb5ba354c21361f8e984d23
Iosif Diana <diana-alexandra.iosif@stefanini.com>
2021-11-16 11:50:47 +0200

    add specific icons for categories

--- src/containers/Category/CategoryItem.tsx
+++ src/containers/Category/CategoryItem.tsx
@@ -4,6 +4,9 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import CategoryIcon from '@mui/icons-material/Category';
 import Button from '@mui/material/Button';
+import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
 
 import style from './CategoryItem.module.scss';
 import { useHistory } from 'react-router-dom';
@@ -20,11 +23,30 @@ const CategoryItem: FC<Props> = ({ category }) => {
     history.push(Routes.productsInCategories.replace(':category', category));
   }, []);
 
+  let categoryIcon;
+
+  switch (category) {
+    case 'electronics': {
+      categoryIcon = <ElectricalServicesIcon />;
+      break;
+    }
+    case "women's clothing": {
+      categoryIcon = <FemaleIcon />;
+      break;
+    }
+    case "men's clothing": {
+      categoryIcon = <MaleIcon />;
+      break;
+    }
+    default:
+      categoryIcon = <CategoryIcon />;
+  }
+
   return (
     <div className={style.categoryContainer}>
       <Button variant="text" onClick={handleClick}>
         <Paper elevation={3} className={style.categoryItem}>
-          <CategoryIcon />
+          {categoryIcon}
           <Typography variant="subtitle2">{category.toUpperCase()}</Typography>
         </Paper>
       </Button>
